f308f34efe0991939a6d1ca22b8b9a1348c9d306
H. Kamran <hkamran@hkamran.com>
2024-10-02 00:48:46 -0700

    Switch to flat config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,79 @@
-const eslintrc = require("./.eslintrc.js");
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 
-module.exports = eslintrc;
+/** @type{import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
+export default [
+    eslint.configs.recommended,
+    prettierConfig,
+    importPlugin.flatConfigs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        rules: {
+            "no-alert": "error",
+            "no-await-in-loop": "error",
+            "no-return-assign": "error",
+            "no-restricted-syntax": [
+                "error",
+                "FunctionExpression",
+                "LabeledStatement",
+                "WithStatement",
+            ],
+            "no-var": "error",
+            "no-use-before-define": "error",
+            "no-unused-expressions": ["error", { enforceForJSX: true }],
+            "no-param-reassign": [
+                "error",
+                {
+                    props: false,
+                },
+            ],
+            "no-console": "error",
+            "no-underscore-dangle": "error",
+
+            "prefer-const": [
+                "error",
+                {
+                    destructuring: "all",
+                },
+            ],
+            "arrow-body-style": [
+                "error",
+                "as-needed",
+                { requireReturnForObjectLiteral: false },
+            ],
+            "space-before-function-paren": "off",
+
+            "import/first": "error",
+            "import/newline-after-import": "error",
+            "import/no-duplicates": "error",
+            "import/no-unassigned-import": [
+                "error",
+                { allow: ["@/styles/*.css"] },
+            ],
+            "import/no-unresolved": [
+                "error",
+                { ignore: ["@/styles/(.*).css"] },
+            ],
+            "import/extensions": ["error", "never", { css: "always" }],
+            "import/no-dynamic-require": "error",
+            "import/no-cycle": "error",
+            "import/order": [
+                "error",
+                {
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        ["parent", "sibling"],
+                        "type",
+                    ],
+                },
+            ],
+        },
+    },
+];
